818c85f4a98c93993620b821304e4f604b54cf44
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-12-02 18:30:33 +0100

    Add animation exports for bread, lettuce, tomato, cheese, top bread, and fork

---
+++ src/animations/index.js
@@ -0,0 +1,6 @@
+export { animateBreadAppearance } from "./breadAnimation";
+export { animateLettuceAdd } from "./lettuceAnimation";
+export { animateTomatoStagger } from "./tomatoAnimation";
+export { animateCheeseThrow } from "./cheeseAnimation";
+export { animateTopBreadCelebration } from "./topBreadAnimation";
+export { animateForkThrow } from "./forkAnimation";
