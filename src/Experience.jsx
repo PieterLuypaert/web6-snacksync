1eab920490c6f876910de15628e7209d5f354f03
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-12-02 18:30:47 +0100

    Refactor Experience component to enhance lighting setup and integrate AnimatedSandwich, improving scene rendering

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,18 +1,52 @@
 import { Canvas } from "@react-three/fiber";
 import React from "react";
-import { Boterham } from "./components/Boterham/Boterham";
-
+import { OrbitControls, Environment, ContactShadows } from "@react-three/drei";
+import AnimatedSandwich from "./components/Scene/AnimatedSandwich";
 
 const Experience = () => {
-    return (
-        <>
-        <Canvas>
-            <directionalLight position={[0, 10, 15]} intensity={3}/>
-            <ambientLight intensity={0.5}/>
-            {/* model */}
-            <Boterham scale={0.5} />
-        </Canvas>
-        </>
-    );
+  return (
+    <Canvas
+      camera={{ position: [0, 4, 9], fov: 45 }}
+      shadows
+      gl={{ alpha: true, antialias: true }}
+    >
+      <Environment preset="city" intensity={0.8} />
+
+      <directionalLight
+        position={[5, 10, 7.5]}
+        intensity={1.5}
+        castShadow
+        shadow-bias={-0.001}
+      />
+      <ambientLight intensity={0.4} />
+      <spotLight
+        position={[0, 10, 0]}
+        angle={0.5}
+        penumbra={1}
+        intensity={1}
+        castShadow
+      />
+
+      <AnimatedSandwich />
+
+      <ContactShadows
+        position={[0, -0.5, 0]}
+        opacity={0.4}
+        scale={15}
+        blur={2.5}
+        far={4}
+        color="#000000"
+      />
+
+      <OrbitControls
+        enableZoom={true}
+        enablePan={false}
+        maxPolarAngle={Math.PI / 2} /* Prevent going below the plate */
+        minDistance={4}
+        maxDistance={12}
+      />
+    </Canvas>
+  );
 };
+
 export default Experience;
