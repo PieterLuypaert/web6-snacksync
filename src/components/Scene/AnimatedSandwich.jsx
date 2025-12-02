36f7e7037f64d4cdba6bb9adf2daa3f081b58ab1
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-12-02 18:31:19 +0100

    Add AnimatedSandwich component for dynamic sandwich animations based on form state

---
+++ src/components/Scene/AnimatedSandwich.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useRef } from "react";
+import { Boterham } from "../Boterham/Boterham";
+import useFormStore from "../../store/Formstore";
+import {
+  animateBreadAppearance,
+  animateLettuceAdd,
+  animateTomatoStagger,
+  animateCheeseThrow,
+  animateTopBreadCelebration,
+  animateForkThrow,
+} from "../../animations";
+
+const AnimatedSandwich = () => {
+  const groupRef = useRef();
+  const { values } = useFormStore();
+  const {
+    hasBread = false,
+    hasLettuce = false,
+    hasTomato = false,
+    hasCheese = false,
+    hasTopBread = false,
+    hasFork = false,
+  } = values;
+
+  const showParts = {
+    hasBread,
+    hasLettuce,
+    hasTomato,
+    hasCheese,
+    hasTopBread,
+    hasFork,
+  };
+
+  useEffect(() => {
+    if (!hasBread) return;
+    animateBreadAppearance(groupRef);
+  }, [hasBread]);
+
+  useEffect(() => {
+    if (!hasLettuce || !hasBread) return;
+    animateLettuceAdd(groupRef);
+  }, [hasLettuce, hasBread]);
+
+  useEffect(() => {
+    if (!hasTomato || !hasBread) return;
+    animateTomatoStagger(groupRef);
+  }, [hasTomato, hasBread]);
+
+  useEffect(() => {
+    if (!hasCheese || !hasBread) return;
+    animateCheeseThrow(groupRef);
+  }, [hasCheese, hasBread]);
+
+  useEffect(() => {
+    if (!hasTopBread || !hasBread) return;
+    animateTopBreadCelebration(groupRef);
+  }, [hasTopBread, hasBread]);
+
+  useEffect(() => {
+    if (!hasFork) return;
+    animateForkThrow(groupRef);
+  }, [hasFork]);
+
+  return <Boterham ref={groupRef} scale={0.5} showParts={showParts} />;
+};
+
+export default AnimatedSandwich;
